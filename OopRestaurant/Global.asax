b9ce279008a885cf7b560b949fc7973ddb964023
szajberpiraty <szajberpiraty@gmail.com>
2018-10-19 19:12:33 +0200

    Add project files.

---
+++ OopRestaurant/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OopRestaurant.MvcApplication" Language="C#" %>
---
+++ OopRestaurant/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OopRestaurant.Startup))]
+namespace OopRestaurant
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
