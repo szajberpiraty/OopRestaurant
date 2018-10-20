22e03c0bace678b81ff14c622f73294391c9330b
szajberpiraty <szajberpiraty@gmail.com>
2018-10-20 17:13:02 +0200

    Adatbázis beállítások után

---
+++ OopRestaurant/Migrations/201810201348499_Identity Datamodel.cs
@@ -0,0 +1,16 @@
+namespace OopRestaurant.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class IdentityDatamodel : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
