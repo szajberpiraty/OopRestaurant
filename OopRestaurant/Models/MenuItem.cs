566df2ab9b947c780cbdb57372d20faa6a5a54f3
szajberpiraty <szajberpiraty@gmail.com>
2018-10-22 15:58:05 +0200

    Modell bővítése új mezővel, majd a mező kötelezővé tétele

--- OopRestaurant/Models/MenuItem.cs
+++ OopRestaurant/Models/MenuItem.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -12,8 +13,10 @@ namespace OopRestaurant.Models
         /// Az id kötelező, elsődleges kulcs
         /// </summary>
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
         public string Description { get; set; }
-        
+        public int Price { get; set; }
+
     }
 }
